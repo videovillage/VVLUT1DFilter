0c09c3793d0f31fe3a9fdc72a4669b5f540565d2
gregcotten <gregcotten@gmail.com>
2014-07-04 14:15:04 -0500

    Initial Commit

---
+++ VVLUT1DFilter/VVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VVAppDelegate.h
+//  VVLUT1DFilter
+//
+//  Created by Greg Cotten on 7/4/14.
+//  Copyright (c) 2014 Video Village. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VVAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ VVLUT1DFilter/VVAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  VVAppDelegate.m
+//  VVLUT1DFilter
+//
+//  Created by Greg Cotten on 7/4/14.
+//  Copyright (c) 2014 Video Village. All rights reserved.
+//
+
+#import "VVAppDelegate.h"
+
+@implementation VVAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
